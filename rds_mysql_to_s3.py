076faf2e14b7d8581ec66fe93215fd0b43c560ba
Jonathan Thompson <jthompson@aarp.org>
2017-02-20 14:52:03 -0600

    Fix typos

--- rds_mysql_to_s3.py
+++ rds_mysql_to_s3.py
@@ -1,4 +1,4 @@
-# 
+#
 # Copyright 2015 Ryan Holland
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
@@ -7,37 +7,37 @@
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #
 
 import boto3, botocore
 
 ## Set the values below if using Lambda Scheduled Event as an Event Source, otherwise leave empty and send data through the Lambda event payload
-S3BCUKET=''
+S3BUCKET=''
 S3PREFIX=''
-RDSINSANCE=''
+RDSINSTANCE=''
 LOGNAME=''
-LASTRECIEVED=''
+LASTRECEIVED=''
 REGION=''
 
 def lambda_handler(event, context):
 	firstRun = False
 	logFileData = ""
-	if {'BucketName','S3BucketPrefix','RDSInstanceName','LogNamePrefix','lastRecievedFile','Region'}.issubset(event):
-		S3BucketName = event['BucketName']	
+	if {'BucketName','S3BucketPrefix','RDSInstanceName','LogNamePrefix','lastReceivedFile','Region'}.issubset(event):
+		S3BucketName = event['BucketName']
 		S3BucketPrefix = event['S3BucketPrefix']
 		RDSInstanceName = event['RDSInstanceName']
 		logNamePrefix = event['LogNamePrefix']
-		lastRecievedFile = S3BucketPrefix + event['lastRecievedFile']
+		lastReceivedFile = S3BucketPrefix + event['lastReceivedFile']
 		region = event['Region']
 	else:
-		S3BucketName = S3BCUKET
+		S3BucketName = S3BUCKET
 		S3BucketPrefix = S3PREFIX
-		RDSInstanceName = RDSINSANCE
+		RDSInstanceName = RDSINSTANCE
 		logNamePrefix = LOGNAME
-		lastRecievedFile = S3BucketPrefix + LASTRECIEVED
+		lastReceivedFile = S3BucketPrefix + LASTRECEIVED
 		region = REGION
 	RDSclient = boto3.client('rds',region_name=region)
 	S3client = boto3.client('s3',region_name=region)
@@ -53,15 +53,15 @@ def lambda_handler(event, context):
 		else:
 			return "Error: Unable to access bucket name, error: " + e.response['Error']['Message']
 	try:
-		S3response = S3client.get_object(Bucket=S3BucketName, Key=lastRecievedFile)
+		S3response = S3client.get_object(Bucket=S3BucketName, Key=lastReceivedFile)
 	except botocore.exceptions.ClientError as e:
 		error_code = int(e.response['ResponseMetadata']['HTTPStatusCode'])
 		if error_code == 404:
 			print("It appears this is the first log import, all files will be retrieved from RDS")
 			firstRun = True
 		else:
-			return "Error: Unable to access lastRecievedFile name, error: " + e.response['Error']['Message']
-	
+			return "Error: Unable to access lastReceivedFile name, error: " + e.response['Error']['Message']
+
 	if firstRun == False:
 		lastWrittenTime = int(S3response['Body'].read(S3response['ContentLength']))
 		print("Found marker from last log download, retrieving log files with lastWritten time after %s" % str(lastWrittenTime))
@@ -83,18 +83,8 @@ def lambda_handler(event, context):
 				return "Error writting object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
 			print("Writting log file %s to S3 bucket %s" % (objectName,S3BucketName))
 	try:
-		S3response = S3client.put_object(Bucket=S3BucketName, Key=lastRecievedFile, Body=str.encode(str(lastWrittenThisRun)))			
+		S3response = S3client.put_object(Bucket=S3BucketName, Key=lastReceivedFile, Body=str.encode(str(lastWrittenThisRun)))
 	except botocore.exceptions.ClientError as e:
-		return "Error writting object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
-	print("Wrote new Last Written Marker to %s in Bucket %s" % (lastRecievedFile,S3BucketName))
+		return "Error writing object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
+	print("Wrote new Last Written Marker to %s in Bucket %s" % (lastReceivedFile,S3BucketName))
 	return "Log file export complete"
-
-
-
-
-
-
-
-
-
-
