bbc6422378c64c54849240ce872b217e505b0b45
Jonathan Thompson <jthompson@aarp.org>
2017-02-22 15:06:46 -0600

    Update for Lambda env vars

--- rds_mysql_to_s3.py
+++ rds_mysql_to_s3.py
@@ -5,22 +5,24 @@
 #
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #
 
+import os
 import boto3, botocore
 
-## Set the values below if using Lambda Scheduled Event as an Event Source, otherwise leave empty and send data through the Lambda event payload
-S3BUCKET=''
-S3PREFIX=''
-RDSINSTANCE=''
-LOGNAME=''
-LASTRECEIVED=''
-REGION=''
+
+S3BUCKET = os.environ['S3BUCKET']
+S3PREFIX = os.environ['S3PREFIX']
+RDSINSTANCE = os.environ['RDSINSTANCE']
+LOGNAME = os.environ['LOGNAME']
+LASTRECEIVED = os.environ['LASTRECEIVED']
+REGION = os.environ['REGION']
+
 
 def lambda_handler(event, context):
     firstRun = False
@@ -44,6 +46,7 @@ def lambda_handler(event, context):
     dbLogs = RDSclient.describe_db_log_files( DBInstanceIdentifier=RDSInstanceName, FilenameContains=logNamePrefix)
     lastWrittenTime = 0
     lastWrittenThisRun = 0
+
     try:
         S3response = S3client.head_bucket(Bucket=S3BucketName)
     except botocore.exceptions.ClientError as e:
@@ -52,6 +55,7 @@ def lambda_handler(event, context):
             return "Error: Bucket name provided not found"
         else:
             return "Error: Unable to access bucket name, error: " + e.response['Error']['Message']
+
     try:
         S3response = S3client.get_object(Bucket=S3BucketName, Key=lastReceivedFile)
     except botocore.exceptions.ClientError as e:
@@ -65,6 +69,7 @@ def lambda_handler(event, context):
     if firstRun == False:
         lastWrittenTime = int(S3response['Body'].read(S3response['ContentLength']))
         print("Found marker from last log download, retrieving log files with lastWritten time after %s" % str(lastWrittenTime))
+
     for dbLog in dbLogs['DescribeDBLogFiles']:
         if ( int(dbLog['LastWritten']) > lastWrittenTime ) or firstRun:
             print("Downloading log file: %s found and with LastWritten value of: %s " % (dbLog['LogFileName'],dbLog['LastWritten']))
@@ -81,10 +86,13 @@ def lambda_handler(event, context):
                 S3response = S3client.put_object(Bucket=S3BucketName, Key=objectName,Body=byteData)
             except botocore.exceptions.ClientError as e:
                 return "Error writing object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
-            print("Writting log file %s to S3 bucket %s" % (objectName,S3BucketName))
+            print("Writing log file %s to S3 bucket %s" % (objectName,S3BucketName))
+
     try:
         S3response = S3client.put_object(Bucket=S3BucketName, Key=lastReceivedFile, Body=str.encode(str(lastWrittenThisRun)))
     except botocore.exceptions.ClientError as e:
         return "Error writing object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
+
     print("Wrote new Last Written Marker to %s in Bucket %s" % (lastReceivedFile,S3BucketName))
+
     return "Log file export complete"
